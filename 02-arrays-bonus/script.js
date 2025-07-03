1849b350c3e3d22013445e708a26bc2d639ab3b0
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-03 18:03:01 +0200

    3/4 bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -14,7 +14,8 @@ const teachers = [
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.length >= 5 ;
+console.log(longNames)
 
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice( 0 , 2 )
@@ -22,7 +23,8 @@ console.log(teachers)
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.indexOf('Fabio');
+console.log(` l'indice di Fabio è ${isFabioPresent}`)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
