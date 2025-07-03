0f6136cce6e769c68e978e129b634b4535654986
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-03 17:03:32 +0200

    step 7

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -37,7 +37,9 @@ teachers.unshift('Sarah')
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
+console.log(`l'indice di Lewis è ${lewisIndex}`) 
+
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
