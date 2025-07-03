44adda048b5dc6259b3d26b5e8d59727622e6cab
CarloMart88 <carlo.martino1988@gmail.com>
2025-07-03 18:51:21 +0200

    fine

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,13 +10,23 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
+// SOLUZIONE IN COMMENTO PER NON CAMBIARE L'ORDINE 
 // const reversedTeachers = teachers.reverse();
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.length >= 5 ;
-console.log(longNames)
+const longNames = []
 
+ for(let i = 0 ; i<teachers.length ; i++){
+
+  let name = teachers[i];
+
+  if (name.length >= 5 ) {
+    longNames.push(name)
+  }
+}
+
+console.log(longNames)
 // 3. Rimuovi 'Ed' dall'array teachers
 teachers.splice( 0 , 2 )
 console.log(teachers)
@@ -27,4 +37,5 @@ const isFabioPresent = teachers.indexOf('Fabio');
 console.log(` l'indice di Fabio è ${isFabioPresent}`)
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.toString();
+console.log(teachersString)
